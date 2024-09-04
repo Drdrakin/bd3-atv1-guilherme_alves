125f9d9b38c10e08b91e3f4555194223d4ce25c8
Guilherme Alves <drdrakino@gmail.com>
2024-09-04 12:29:28 -0300

    fix: Cpf válido

--- Atividade/list-by-cpf.mongodb.js
+++ Atividade/list-by-cpf.mongodb.js
@@ -2,7 +2,7 @@ const database = 'BD3-NoSQL-AtlasMongoDB';
 
 const collection = 'bd3-nosql-atv1';
 
-const condition = {cpf: "23456789012"};
+const condition = {cpf: "18547678901"};
 const exclude = {"_id": 0, "cod_aluno": 0};
 
 use(database);
